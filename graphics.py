464efb68769e53d685838cd246b75efcb8c68d69
dskim731 <dskim731@gmail.com>
2023-01-05 13:14:20 -0600

    cells created

--- graphics.py
+++ graphics.py
@@ -1,10 +1,11 @@
 from tkinter import Tk, BOTH, Canvas
 
 
-class Window():
+class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("Maze Solver")
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
         self.canvas = Canvas(self.root, bg="white", width=width, height=height)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
@@ -12,6 +13,9 @@ class Window():
     def redraw(self):
         self.root.update_idletasks()
         self.root.update()
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.canvas, fill_color)
 
     def to_close(self):
         self.running = True
@@ -21,3 +25,20 @@ class Window():
 
     def close(self):
         self.running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x,
+                           self.p2.y, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
